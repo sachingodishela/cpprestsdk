98bc0189245f60c0438e6710eb18709a722154ac
Sachin Godishela <sachingodishela@outlook.com>
2022-05-28 13:54:21 +0530

    local

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/gsachingoud/Celigo/js-runtime/third_party/cpprestsdk
+# Build directory: /Users/gsachingoud/Celigo/js-runtime/third_party/cpprestsdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Release")
